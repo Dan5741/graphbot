054d666f3e42c2e63f0a999a81c32541a47accda
Daniel <you@example.com>
2018-10-28 15:02:04 -0700

    asdfas

--- static/index.js
+++ static/index.js
@@ -1,58 +1,40 @@
 $(function () {
-dataz = new FormData()
+$("#inner_text").append('<div class="row"> <div class="col text-left"><b>Bot: </b> What type of chart? </div> </div>')
+   
+question_int = 0;
+var dataz =''
+categories = 0;
+inputs = new FormData()
 
-sub1 = 'firefox,25'
-sub2 = 'safari,75'
-main = sub1 + ':' + sub2
-
-dataz.append('title','yo')
-dataz.append('data',main)
-dataz.append('type','pie')
-    $.ajax({
-        type:'POST',
-        url:'http://localhost:5000/send',
-        data:dataz,
-	processData: false,
-	contentType:false,
-        success:function(data){
-            console.log(data)
-source = 'data:image/jpeg;base64,' + data
-$("#hello").attr('src',source)
-        }
-    })
-    $("#inner_text").append('<div class="row"> <div class="col text-left"><b>Bot: </b> What type of chart? </div> </div>')
-    
-    question_int = 0;
-    question_int ++;
-    inputs = []
-    $(document).on('click', '#sendbtn', () => {
-        if ($("#input_text").val() == '')
-        {
-          return;
-        }
+$(document).on('click', '#sendbtn', () => {
+      if ($("#input_text").val() == '')
+      {
+        return;
+      }
 
            
             switch (question_int) {
                 case 0:
-                    question = 'What chart do you want?'
+                   question = 'What type of chart?'
                    sendbtn()
                    break;
                 case 1:
-               
-                question = 'What type of ' +  $("#input_text").val() + "?"
+                question = 'What is the title of your chart?'
                 sendbtn()
                 break;
                 case 2:
-                question = 'What is your data?'
+                question = 'How many categories will you have?'
                 sendbtn()
                 break;
                 case 3:
-                question = 'Here is your chart:'
-                question_int = -1;
-                
-                inputs = []
+                question = 'What is the category name?'
                 sendbtn()
                 break;
+		case 4:
+                question = 'Enter the data separated by commas'
+                sendbtn()
+                break;
+		
                 
             }
             
@@ -67,18 +49,76 @@ $("#hello").attr('src',source)
 })
 
 sendbtn = () =>{
-    
-    inputs.push($("#input_text").val())
-    console.log(inputs[0])
-    console.log(...inputs)
-    $("#inner_text").append('<div class="row text-right"> <div class="col"><b>You: </b>'+ $("#input_text").val() + ' </div> </div>')
-    $("#inner_text").append('<div class="row"> <div class="col text-left"><b>Bot: </b>'+ question +'</div> </div>')
-    console.log('<div class="row"> <div class="col text-left"><b>Bot: </b>'+ question +'</div> </div>')
-   
-    $("#input_text").val('')
-
-    question_int ++ 
-
-    var objDiv = document.getElementById("inner");
-    objDiv.scrollTop = objDiv.scrollHeight;
+   $("#inner_text").append('<div class="row"> <div class="col text-left"><b>You: </b>'+ $("#input_text").val('') +'</div> </div>')
+   $("#inner_text").append('<div class="row"> <div class="col text-left"><b>Bot: </b>'+ question +'</div> </div>')
+  
+   if (question_int == 0 )
+{
+	inputs.append('type',$("#input_text").val().toLowerCase()))
 }
+else if (question_int ==1 )
+{
+	inputs.append('title',$("#input_text").val())	
+}
+else if (question_int ==2 )
+{
+		
+categories = $("#input_text").val()
+}
+else if (question_int == 3 )
+{
+	if (dataz == ''){
+dataz = $("#input_text").val()
+}
+else{
+dataz = dataz + ':' + $("#input_text").val()
+}
+}
+else if (question_int == 4 )
+{
+categories = categories - 1
+if categories > 0
+{
+	question_int = 2
+	dataz = ',' + $("#input_text").val()
+	
+}
+else if (categories == 0){
+	dataz = ',' + $("#input_text").val()
+	inputs.append('data',dataz)
+$.ajax({
+        type:'POST',
+        url:'http://localhost:5000/send',
+        data:inputs,
+	processData: false,
+	contentType:false,
+        success:function(data){
+            console.log(data)
+source = 'data:image/jpeg;base64,' + data
+$("#hello").attr('src',source)
+        }
+    })	
+}
+}
+
+
+
+
+
+    		question_int ++ 
+	 $("#input_text").val('')
+    
+// scroll down
+var objDiv = document.getElementById("inner");
+objDiv.scrollTop = objDiv.scrollHeight;
+}
+
+dataz = new FormData()
+sub1 = 'firefox,25'
+sub2 = 'safari,75'
+main = sub1 + ':' + sub2
+
+dataz.append('title','yo')
+dataz.append('data',main)
+dataz.append('type','pie')
+    
